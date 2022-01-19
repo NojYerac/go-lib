a93a9c034c6d17f3592667d89046364d51bc766d
Jon Carey <noj.yerac@gmail.com>
2022-01-19 20:19:48 +0000

    fix grpc

--- go.mod
+++ go.mod
@@ -8,21 +8,24 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/grpc-ecosystem/go-grpc-middleware/providers/zerolog/v2 v2.0.0-rc.2
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.0-20200501113911-9a95f0fdbfea
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.10.4
 	github.com/mitchellh/mapstructure v1.4.3
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
 	github.com/prometheus/client_golang v1.11.0
 	github.com/rs/zerolog v1.26.1
-	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.10.1
 	github.com/stretchr/testify v1.7.0
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.28.0
 	google.golang.org/grpc v1.43.0
 	google.golang.org/grpc/examples v0.0.0-20220113003412-a002994200f3
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
-	source.rad.af/libs/lib-grpc v0.0.0-20211231210304-18e370fbd529
-	source.rad.af/libs/lib-http v0.0.0-20220108043822-7f9989ab3aa1
 )
---
+++ pkg/transport/grpc/grpclogger.go
@@ -0,0 +1,79 @@
+package grpc
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog"
+	"google.golang.org/grpc/grpclog"
+)
+
+func replaceGRPCLogger(l *zerolog.Logger) {
+	gl := &grpcV2Logger{*l}
+	grpclog.SetLoggerV2(gl)
+}
+
+var _ grpclog.LoggerV2 = &grpcV2Logger{}
+
+type grpcV2Logger struct {
+	zerolog.Logger
+}
+
+func (gl *grpcV2Logger) Info(args ...interface{}) {
+	gl.Logger.Debug().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Infoln(args ...interface{}) {
+	gl.Logger.Debug().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Infof(format string, args ...interface{}) {
+	gl.Logger.Debug().Msgf(format, args...)
+}
+
+func (gl *grpcV2Logger) Warning(args ...interface{}) {
+	gl.Logger.Warn().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Warningln(args ...interface{}) {
+	gl.Logger.Warn().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Warningf(format string, args ...interface{}) {
+	gl.Logger.Warn().Msgf(format, args...)
+}
+
+func (gl *grpcV2Logger) Error(args ...interface{}) {
+	gl.Logger.Error().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Errorln(args ...interface{}) {
+	gl.Logger.Error().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Errorf(format string, args ...interface{}) {
+	gl.Logger.Fatal().Msgf(format, args...)
+}
+
+func (gl *grpcV2Logger) Fatal(args ...interface{}) {
+	gl.Logger.Fatal().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Fatalln(args ...interface{}) {
+	gl.Logger.Fatal().Msg(fmt.Sprint(args...))
+}
+
+func (gl *grpcV2Logger) Fatalf(format string, args ...interface{}) {
+	gl.Logger.Fatal().Msgf(format, args...)
+}
+
+func (gl *grpcV2Logger) V(l int) bool {
+	lvl := gl.Logger.GetLevel()
+	switch lvl {
+	case zerolog.TraceLevel, zerolog.DebugLevel:
+		return true
+	case zerolog.Disabled:
+		return false
+	}
+
+	return int(lvl) <= l+1
+}
