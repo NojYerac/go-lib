88ced57b0ee8bd7537b6a1b5253cca0ba2188b89
Jon Carey <noj.yerac@gmail.com>
2024-01-27 07:01:52 +0000

    Test token

--- pkg/jwt/config.go
+++ pkg/jwt/config.go
@@ -1,4 +1,4 @@
-package token
+package jwt
 
 import (
 	"time"
@@ -8,16 +8,14 @@ import (
 
 type Configuration struct {
 	AccessTokenTTL   time.Duration `config:"access_token_ttl" validate:"required"`
-	RefreshTokenTTL  time.Duration `config:"refresh_token_ttl"`
 	JWTSigningMethod string        `config:"jwt_signing_method" validate:"required,oneof=ES256 ES384 ES512"`
-	JWTPrivateKey    string        `config:"jwt_private_key" validate:"required,priv_ec_key"`
-	JWTPublicKey     string        `config:"jwt_public_key" validate:"required,pub_key"`
+	JWTPrivateKey    string        `config:"jwt_private_key" validate:"required_without=JWTPublicKey,priv_ec_key"`
+	JWTPublicKey     string        `config:"jwt_public_key" validate:"required_without=JWTPrivateKey,pub_key"`
 }
 
 func NewConfiguration() *Configuration {
 	return &Configuration{
 		AccessTokenTTL:   15 * time.Minute,
-		RefreshTokenTTL:  7 * 24 * time.Hour,
 		JWTSigningMethod: jwt.SigningMethodES256.Name,
 	}
 }
